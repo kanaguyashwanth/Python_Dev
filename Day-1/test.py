ef5bcebe9215862a702b2d0afde40f7ded8251b6
Yashwanth K <kanaguyashwanth@gmail.com>
2024-02-04 23:53:42 +0530

    Add files via upload

---
+++ Day-1/test.py
@@ -0,0 +1,14 @@
+# CHALLENGE: Write a program that switches the values stored in the variables a and b.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+c = a
+a = b
+b = c
+
+
+print(a)
+print(b)
