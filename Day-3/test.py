20387d0ca6056906f6473673a554a1fe07862d35
Yashwanth K <kanaguyashwanth@gmail.com>
2024-02-06 23:43:59 +0530

    Add files via upload

---
+++ Day-3/test.py
@@ -0,0 +1,15 @@
+# CODING EXERCISE - Odd or Even Exercise (Using Modulo)
+# NESTED if STATEMENTS AND elif STATEMENTS:
+
+'''
+Welcome to the rollercoaster!
+What is your height in cm? 121
+You can ride the rollercoaster
+
+CONDITION: Height > 120 cm
+           age  < 12 ---> Pay $5
+           age <= 18 ---> Pay $7
+           age  > 18 ---> Pay $12
+'''
+
+
