a9578203a8b5c9543da452867ce327a14b3809e3
Yashwanth K <kanaguyashwanth@gmail.com>
2024-02-08 23:32:02 +0530

    Add files via upload

---
+++ Day-5/README.md
@@ -0,0 +1,7 @@
+# Python_Programming_101
+
+Day - 1: Completed
+Day - 2: Completed
+Day - 3: Completed
+Day - 4: Completed
+Day - 5: Scheduled
---
+++ Day-5/test.py
@@ -0,0 +1,47 @@
+# for loop
+'''
+SYNTAX:
+
+for var_name in list_name:
+    print (var_name)
+'''
+
+
+
+# # Example_1: [Printing the List using for loop]
+# fruits = ['apple', 'banana', 'orange']
+# for x in fruits:
+#     print (x)
+
+
+
+# # Example_2: [Average student height]
+# student_heights = input("Input a list of student heights ").split()     #This would split the 'space' between each entry
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+#
+# #Write your code below this row
+# sum_heights = 0
+# for n in range(0, len(student_heights)):
+#     sum_heights += student_heights[n]
+#
+# average_height = sum_heights / len(student_heights)
+# print(average_height)
+
+
+
+# Example_3:
+# student_scores = input("Input list of student scores: ").split()            # This would split the space between each entry
+# # printing the student scores
+# for x in range (len(student_scores)):
+#     print(student_scores[x])
+#
+# # deciding the highest score
+# highest_score = student_scores[0]
+# for x in range (len(student_scores)):
+#     if (student_scores[x] > highest_score):
+#         highest_score = student_scores[x]
+#
+# print(f'The highest score is: {highest_score}')
+
+
